bf4ba68dae3bb7102ad4802de61f7989576e2d8d
lidong.lv <lidong.lv@westsnow.cn>
2023-11-09 09:55:24 +0800

    support x86 yara

--- Libraries/dnYara.Interop/Interops/Types/YR_RULE.cs
+++ Libraries/dnYara.Interop/Interops/Types/YR_RULE.cs
@@ -3,23 +3,30 @@ using System.Runtime.InteropServices;
 
 namespace dnYara.Interop
 {
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Explicit)]
     public struct YR_RULE
     {
+        [FieldOffset(0)]
         // Global flags
         public int flags;
 
+        [FieldOffset(4)]
         // Number of atoms generated for this rule.
         public int num_atoms;
 
+        [FieldOffset(12)]
         public IntPtr identifier;
 
+        [FieldOffset(20)]
         public IntPtr tags;
 
+        [FieldOffset(28)]
         public IntPtr metas;
 
+        [FieldOffset(36)]
         public IntPtr strings;
 
+        [FieldOffset(44)]
         public IntPtr ns;
     }
 
